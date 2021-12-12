67bbba6e4c6fe1110b7e181e4ea6a17e1f885fd3
Jeff Wagner <jeff.wagner@swipesense.com>
2021-12-12 17:53:38 -0600

    Exercise 5 - styling

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -1,6 +1,7 @@
 // Styling
 // http://localhost:3000/isolated/exercise/05.js
 
+import { BackgroundColor } from 'chalk'
 import * as React from 'react'
 import '../box-styles.css'
 
@@ -12,14 +13,24 @@ import '../box-styles.css'
 
 // 🐨 add a style prop to each of them as well so their background color
 // matches what the text says it should be as well as `fontStyle: 'italic'`
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
+const Box = (props) => {
+  return <div 
+    className={`box box--${props.size}`} 
+    style={{backgroundColor: props.backgroundColor, fontStyle: props.fontStyle}}
+  >small lightblue box</div>
+}
+
+const mediumBox = <div className="box box--medium" style={{backgroundColor: 'pink', fontStyle: 'initial'}}>medium pink box</div>
+const largeBox = <div className="box box--large" style={{backgroundColor: 'orange', fontStyle: 'oblique'}}>large orange box</div>
 
 function App() {
   return (
     <div>
-      {smallBox}
+      {Box({
+        size: 'small',
+        backgroundColor: 'lightblue',
+        fontStyle: 'initial'
+      })}
       {mediumBox}
       {largeBox}
     </div>

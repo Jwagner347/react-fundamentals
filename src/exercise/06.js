9ee573fbe9a8d24a99c5674cb38d0693e80a9bfc
Jeff Wagner <jeff.wagner@swipesense.com>
2021-12-12 21:04:37 -0600

    Exercise 6 - forms - ex credit 2

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -5,13 +5,14 @@ import * as React from 'react'
 import {useRef} from 'react'
 
 function UsernameForm({onSubmitUsername}) {
+  const [error, setError] = React.useState(null)
+
   // 🐨 add a submit event handler here (`handleSubmit`).
-  // const handleSubmit = (event) => {
-  //   event.preventDefault()
-  //   const val = event.target.elements[0].value
-  //   console.log('val is: ', val);
-  //   onSubmitUsername(val)
-  // }
+  const handleChange = (event) => {
+    const value = event.target.value
+    const isValid = value === value.toLowerCase()
+    setError(isValid ? null : 'Username must be lower case')
+  }
 
   const inputEl = useRef(null);
 
@@ -25,9 +26,10 @@ function UsernameForm({onSubmitUsername}) {
     <form onSubmit={onButtonClick}>
       <div>
         <label htmlFor="myId">Username:</label>
-        <input ref={inputEl} id="myId" type="text" />
+        <input onChange={handleChange} ref={inputEl} id="myId" type="text" />
       </div>
-      <button type="submit">Submit</button>
+        {!!error && (<div role="alert">{error}</div>)}
+      <button disabled={!error ? false : true } type="submit">Submit</button>
     </form>
   )
 }
